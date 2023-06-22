6dcd4949386d5ab82f8df2b032db786fcd003772
Andrew Thompson <andrew@hijacked.us>
2023-06-22 12:30:03 -0700

    Make it work with signal-server as a crate

--- src/daemon_handle.rs
+++ src/daemon_handle.rs
@@ -1,27 +1,46 @@
 use super::{Error, Result};
 use rust_decimal::Decimal;
+use rust_decimal_macros::dec;
 use std::process::Stdio;
 use tokio::{
     io::{AsyncBufReadExt, AsyncWriteExt, BufReader},
     process::Command,
 };
 
+use std::collections::HashMap;
+
+use axum::{
+    http::{StatusCode},
+};
+
+use std::path::Path;
+
 pub struct DaemonHandle {
-    _child: tokio::process::Child,
-    stdin: tokio::process::ChildStdin,
-    stdout: BufReader<tokio::process::ChildStdout>,
+    //_child: tokio::process::Child,
+    //stdin: tokio::process::ChildStdin,
+    //stdout: BufReader<tokio::process::ChildStdout>,
 }
 
 #[derive(Debug, serde::Serialize)]
-pub struct Response {
-    pub path_loss: Decimal,
-    pub received_power: Decimal,
-    pub field_strength: Decimal,
+pub struct PathResponse {
+    pub path_loss: f64,
+    pub received_power: f64,
+    pub field_strength: f64,
+    pub distance: Vec<f64>,
+    pub reference: Vec<f64>,
+    pub fresnel: Vec<f64>,
+    pub fresnel60: Vec<f64>,
+    pub curvature: Vec<f64>,
+    pub profile: Vec<f64>,
+}
+
+#[derive(Debug, serde::Serialize)]
+pub struct H3PlotResponse {
+    pub hexes: HashMap<String, f64>
 }
 
 #[derive(Debug, serde::Deserialize)]
-pub struct Params {
-    sdf: String,
+pub struct PathParams {
     lat: Decimal,
     lon: Decimal,
     txh: Decimal,
@@ -31,61 +50,123 @@ pub struct Params {
     rt: Decimal,
     dbm: bool,
     m: bool,
-    o: String,
-    #[serde(rename = "R")]
-    r: usize,
-    res: Decimal,
     pm: Decimal,
     rla: Decimal,
     rlo: Decimal,
+    pe: Option<u8>,
+    gc: Option<u16>,
+}
+
+#[derive(Debug, serde::Deserialize)]
+pub struct PlotParams {
+    lat: Decimal,
+    lon: Decimal,
+    txh: Decimal,
+    f: Decimal,
+    erp: Decimal,
+    rxh: Decimal,
+    rt: Decimal,
+    dbm: bool,
+    t: Option<bool>,
+    m: bool,
+    #[serde(rename = "R")]
+    r: usize,
+    pm: Decimal,
+    pe: Option<u8>,
+    gc: Option<u16>,
+}
+
+#[derive(Debug, serde::Deserialize)]
+pub struct H3PlotParams {
+    lat: f64,
+    lon: f64,
+    txh: Decimal,
+    f: Decimal,
+    erp: Decimal,
+    rxh: Decimal,
+    rt: f64,
+    dbm: bool,
+    pm: Decimal,
+    // should be 0-15 or whatever h3 allows
+    res: u8,
+    pe: Option<u8>,
+    gc: Option<u16>,
 }
 
 impl DaemonHandle {
     pub async fn new() -> Result<DaemonHandle> {
-        let exec = std::env::var("SS_EXEC").map_err(|_| Error::NoExec)?;
-        let mut child = if let Ok(sdf) = std::env::var("SS_SDF") {
-            Command::new(&exec)
-                .arg("-daemon")
-                .arg("-sdf")
-                .arg(&sdf)
-                .stdout(Stdio::piped())
-                .stdin(Stdio::piped())
-                .spawn()?
+        if let Ok(sdf) = std::env::var("SS_SDF") {
+            signal_server::init(Path::new(&sdf), false);
+            Ok(DaemonHandle {})
         } else {
-            Command::new(&exec)
-                .arg("-daemon")
-                .stdout(Stdio::piped())
-                .stdin(Stdio::piped())
-                .spawn()?
-        };
-
-        let stdin = child.stdin.take().unwrap();
-        let stdout = BufReader::new(child.stdout.take().unwrap());
-
-        Ok(DaemonHandle {
-            _child: child,
-            stdin,
-            stdout,
-        })
+            signal_server::init(Path::new("/tmp/doesnotexist"), true);
+            Ok(DaemonHandle {})
+        }
     }
 
-    pub async fn run(&mut self, params: Params) -> Result<Response> {
+    pub async fn path(&self, params: PathParams) -> Result<PathResponse> {
         let params = params.to_stdout_string();
-        self.stdin.write_all(params.as_bytes()).await.unwrap();
-        self.stdin.flush().await.unwrap();
-        let mut response = String::new();
-        self.stdout.read_line(&mut response).await?;
-        trim_newline(&mut response);
-        let decimal = response
-            .split(' ')
-            .map(|s| s.parse::<Decimal>().unwrap())
-            .collect::<Vec<Decimal>>();
-        Ok(Response {
-            path_loss: decimal[0],
-            received_power: decimal[1],
-            field_strength: decimal[2],
+        let report = signal_server::call_sigserve(&params).unwrap();
+        Ok(PathResponse {
+            path_loss: report.loss,
+            received_power: report.dbm,
+            field_strength: report.field_strength,
+            distance: report.distancevec,
+            reference: report.referencevec,
+            fresnel: report.fresnelvec,
+            fresnel60: report.fresnel60vec,
+            curvature: report.curvaturevec,
+            profile: report.profilevec,
         })
     }
+
+    pub async fn h3plot(&self, params: H3PlotParams) -> Result<H3PlotResponse> {
+        let res = match params.res {
+            1 => h3o::Resolution::One,
+            2 => h3o::Resolution::Two,
+            3 => h3o::Resolution::Three,
+            4 => h3o::Resolution::Four,
+            5 => h3o::Resolution::Five,
+            6 => h3o::Resolution::Six,
+            7 => h3o::Resolution::Seven,
+            8 => h3o::Resolution::Eight,
+            9 => h3o::Resolution::Nine,
+            10 => h3o::Resolution::Ten,
+            // below 10 is smaller than 3 arc seconds
+            //11 => h3o::Resolution::Eleven,
+            //12 => h3o::Resolution::Twelve,
+            _ => return Err(Error::Axum("bad resolution".into()))
+        };
+        let ll = h3o::LatLng::new(params.lat as f64, params.lon as f64).unwrap();
+        let cell = ll.to_cell(res);
+        let mut i = 0;
+        let mut hexes = HashMap::new();
+        loop {
+            let cells = cell.grid_ring_fast(i).collect::<Option<Vec<_>>>()
+                .unwrap_or_default();
+            let mut found = false;
+            for cell in cells {
+                let latlng = h3o::LatLng::from(cell);
+                let paramstr = params.to_stdout_string(latlng.lat(), latlng.lng());
+                let report = signal_server::call_sigserve(&paramstr).unwrap();
+                if (report.dbm > params.rt) {
+                    hexes.insert(format!("{}", cell), report.dbm);
+                    found = true;
+                }
+            }
+            if !found {
+                break Ok(H3PlotResponse {hexes: hexes});
+            }
+            i+= 1;
+        }
+    }
+
+    pub async fn plot(&self, params: PlotParams) -> Result<Vec<u8>> {
+        let params = params.to_stdout_string();
+        let report = signal_server::call_sigserve(&params).unwrap();
+        Ok(report.image_data)
+    }
+
 }
 
 fn trim_newline(s: &mut String) {
@@ -97,10 +178,9 @@ fn trim_newline(s: &mut String) {
     }
 }
 
-impl Params {
+impl PathParams {
     fn to_stdout_string(&self) -> String {
-        let mut output = format!("-sdf {}", self.sdf);
-        output.push_str(&format!(" -lat {}", self.lat));
+        let mut output = format!(" -lat {}", self.lat);
         output.push_str(&format!(" -lon {}", self.lon));
         output.push_str(&format!(" -txh {}", self.txh));
         output.push_str(&format!(" -f {}", self.f));
@@ -113,16 +193,77 @@ impl Params {
         if self.m {
             output.push_str(" -m");
         }
-        output.push_str(&format!(" -o {}", self.o));
-        output.push_str(&format!(" -R {}", self.r));
-        output.push_str(&format!(" -res {}", self.res));
         output.push_str(&format!(" -pm {}", self.pm));
         output.push_str(&format!(" -rla {}", self.rla));
         output.push_str(&format!(" -rlo {}", self.rlo));
+        if self.pe.is_some() {
+            output.push_str(&format!(" -pe {}", self.pe.unwrap()));
+        }
+        if self.gc.is_some() {
+            output.push_str(&format!(" -gc {}", self.gc.unwrap()));
+        }
         output.push_str("\r\n");
         output
     }
 }
+
+impl PlotParams {
+    fn to_stdout_string(&self) -> String {
+        let mut output = format!(" -lat {}", self.lat);
+        output.push_str(&format!(" -lon {}", self.lon));
+        output.push_str(&format!(" -txh {}", self.txh));
+        output.push_str(&format!(" -f {}", self.f));
+        output.push_str(&format!(" -erp {}", self.erp));
+        output.push_str(&format!(" -rxh {}", self.rxh));
+        output.push_str(&format!(" -rt {}", self.rt));
+        if self.dbm {
+            output.push_str(" -dbm");
+        }
+        if self.m {
+            output.push_str(" -m");
+        }
+        output.push_str(&format!(" -pm {}", self.pm));
+        output.push_str(&format!(" -R {}", self.r));
+        if self.t.is_some() {
+            output.push_str(" -t");
+        }
+        if self.pe.is_some() {
+            output.push_str(&format!(" -pe {}", self.pe.unwrap()));
+        }
+        if self.gc.is_some() {
+            output.push_str(&format!(" -gc {}", self.gc.unwrap()));
+        }
+        output.push_str("\r\n");
+        output
+    }
+}
+
+impl H3PlotParams {
+    fn to_stdout_string(&self, lat: f64, lng: f64) -> String {
+        let mut output = format!(" -lat {}", self.lat);
+        output.push_str(&format!(" -lon {}", self.lon));
+        output.push_str(&format!(" -rla {}", lat));
+        output.push_str(&format!(" -rlo {}", lng));
+        output.push_str(&format!(" -txh {}", self.txh));
+        output.push_str(&format!(" -f {}", self.f));
+        output.push_str(&format!(" -erp {}", self.erp));
+        output.push_str(&format!(" -rxh {}", self.rxh));
+        output.push_str(&format!(" -rt {}", self.rt));
+        if self.dbm {
+            output.push_str(" -dbm");
+        }
+        output.push_str(&format!(" -pm {}", self.pm));
+        if self.pe.is_some() {
+            output.push_str(&format!(" -pe {}", self.pe.unwrap()));
+        }
+        if self.gc.is_some() {
+            output.push_str(&format!(" -gc {}", self.gc.unwrap()));
+        }
+        output.push_str("\r\n");
+        output
+    }
+}
+
 
 #[cfg(test)]
 mod test {
@@ -131,8 +272,7 @@ mod test {
 
     #[test]
     fn test_params_to_string() {
-        let params = Params {
-            sdf: "hgt".to_string(),
+        let params = PathParams {
             lat: Decimal::from_str("44.73566").unwrap(),
             lon: Decimal::from_str("-68.82446").unwrap(),
             txh: Decimal::from_str("4").unwrap(),
@@ -144,12 +284,11 @@ mod test {
             m: true,
             o: "test4".to_string(),
             r: 2,
-            res: Decimal::from_str("1200").unwrap(),
             pm: Decimal::from_str("4").unwrap(),
             rla: Decimal::from_str("44.73436").unwrap(),
             rlo: Decimal::from_str("-68.81993").unwrap(),
         };
         let params_string = params.to_stdout_string();
-        assert_eq!(params_string, "-sdf hgt -lat 44.73566 -lon -68.82446 -txh 4 -f 900 -erp 5 -rxh 2 -rt -90 -dbm -m -o test4 -R 2 -res 1200 -pm 4 -rla 44.73436 -rlo -68.81993\n")
+        assert_eq!(params_string, "-sdf hgt -lat 44.73566 -lon -68.82446 -txh 4 -f 900 -erp 5 -rxh 2 -rt -90 -dbm -m -o test4 -res 1200 -pm 4 -rla 44.73436 -rlo -68.81993\n")
     }
 }
--- src/server.rs
+++ src/server.rs
@@ -1,10 +1,15 @@
 use super::*;
-use axum::{extract::Query, http::StatusCode, response, routing::get, Extension, Router};
+use axum::{extract::Query, http::{header, StatusCode}, response, routing::get, Extension, Router, response::{IntoResponse},};
 use axum_auth::AuthBearer;
+use axum::body::StreamBody;
 use daemon_handle::DaemonHandle;
 use serde_json::{json, Value};
 use std::sync::Arc;
 use tokio::sync::Mutex;
+
+use std::io;
+
+use futures_util::stream::{self, Stream};
 
 pub type HandlerResult = std::result::Result<response::Json<Value>, (StatusCode, String)>;
 
@@ -13,18 +18,13 @@ pub struct Server {}
 
 impl Server {
     pub async fn run(self) -> Result {
-        let daemon: Arc<Mutex<DaemonHandle>> = Arc::new(Mutex::new(DaemonHandle::new().await?));
+        let daemon: Arc<DaemonHandle> = Arc::new(DaemonHandle::new().await?);
 
-        let app = if let Ok(env_token) = std::env::var("AUTH_TOKEN") {
-            Router::new()
-                .route("/v1/stdin", get(auth_stdin_handler))
-                .layer(Extension(daemon))
-                .layer(Extension(env_token))
-        } else {
-            Router::new()
-                .route("/v1/stdin", get(stdin_handler))
-                .layer(Extension(daemon))
-        };
+        let app = Router::new()
+            .route("/v1/path", get(path_handler))
+            .route("/v1/plot", get(plot_handler))
+            .route("/v1/h3plot", get(h3plot_handler))
+            .layer(Extension(daemon));
 
         let server_endpoint = std::env::var("PORT").unwrap_or("3000".to_string());
         println!("Binding to port {}...", server_endpoint);
@@ -43,27 +43,54 @@ impl From<Error> for HandlerResult {
     }
 }
 
-pub async fn auth_stdin_handler(
-    Extension(daemon): Extension<Arc<Mutex<DaemonHandle>>>,
-    Extension(env_token): Extension<String>,
-    query: Query<daemon_handle::Params>,
-    AuthBearer(token): AuthBearer
-
+pub async fn path_handler(
+    Extension(daemon): Extension<Arc<DaemonHandle>>,
+    //Extension(env_token): Extension<String>,
+    query: Query<daemon_handle::PathParams>,
+    //AuthBearer(token): AuthBearer
 ) -> HandlerResult {
-    if token != env_token {
-        return Err((StatusCode::UNAUTHORIZED, "Unauthorized".to_string()));
-    }
-    stdin_handler(Extension(daemon), query).await
-}
-
-pub async fn stdin_handler(
-    Extension(daemon): Extension<Arc<Mutex<DaemonHandle>>>,
-    query: Query<daemon_handle::Params>,
-) -> HandlerResult {
-    let mut daemon = daemon.lock().await;
     let query = query.0;
     daemon
-        .run(query)
+        .path(query)
+        .await
+        .map(|r| {
+            response::Json(json!({
+                "status": "success",
+                "data": r}))
+        })
+        .map_err(|e| (StatusCode::INTERNAL_SERVER_ERROR, e.to_string()))
+}
+
+pub async fn plot_handler(
+    Extension(daemon): Extension<Arc<DaemonHandle>>,
+    //Extension(env_token): Extension<String>,
+    query: Query<daemon_handle::PlotParams>,
+    //AuthBearer(token): AuthBearer
+) -> impl IntoResponse {
+    //if token != env_token {
+    //    return Err((StatusCode::UNAUTHORIZED, "Unauthorized".to_string()));
+    //}
+    let query = query.0;
+    let png = daemon
+        .plot(query)
+        .await.unwrap();
+    
+    let headers = [
+                          (header::CONTENT_TYPE, "image/png"),
+    ];
+
+    (headers, png)
+}
+
+pub async fn h3plot_handler(
+    Extension(daemon): Extension<Arc<DaemonHandle>>,
+    //Extension(env_token): Extension<String>,
+    query: Query<daemon_handle::H3PlotParams>,
+    //AuthBearer(token): AuthBearer
+) -> HandlerResult {
+    let query = query.0;
+    daemon
+        .h3plot(query)
         .await
         .map(|r| {
             response::Json(json!({
